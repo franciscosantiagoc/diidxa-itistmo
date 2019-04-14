32167f8c38d137a89470d3bee9ea4e9f7d0f81c0
Francisco Santiago de la Cruz <francisco.santiago@itistmo.edu.mx>
2019-04-14 12:00:57 -0500

    Trabajando en errores... 1.- Correccion de busqueda en versiones menores o igual a 6.0 2.- Correccion de interacción entre busqueda y diccionario 3.- Conectando sugerencias con firebase

---
+++ app/src/main/java/mx/edu/itistmo/diidxaza/DatosSugerencia.java
@@ -0,0 +1,47 @@
+package mx.edu.itistmo.diidxaza;
+
+public class DatosSugerencia {
+    private String palabra;
+    private String traduccion;
+    private String nombre;
+    private String email;
+
+    public DatosSugerencia(String palabra, String traduccion, String nombre, String email) {
+        this.palabra = palabra;
+        this.traduccion = traduccion;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public String getTraduccion() {
+        return traduccion;
+    }
+
+    public void setTraduccion(String traduccion) {
+        this.traduccion = traduccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
